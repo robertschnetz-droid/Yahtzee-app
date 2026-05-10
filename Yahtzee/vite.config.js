681d68265b32cc595da22ab50e3263ac4d06b1cc
Robert Schnetz <robertschnetz@gmail.com>
2026-05-10 23:54:18 +0200

    add pwa support

--- Yahtzee/vite.config.js
+++ Yahtzee/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Yahtzee',
+        short_name: 'Yahtzee',
+        description: 'Yahtzee score app',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
